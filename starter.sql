2ef9b088aa1003ede96d82450af31c8751e6d8c0
1U25CS011-ARULGNANAPARANJOTHI <jaspritarul@gmail.com>
2026-09-25 12:29:10 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,26 +1,15 @@
-
 CREATE DATABASE IF NOT EXISTS CollegeDB;
-
 USE CollegeDB;
 
 DROP TABLE IF EXISTS Student;
-
-CREATE TABLE Student (
-    StudentID INT PRIMARY KEY,
-    StudentName VARCHAR(20) NOT NULL,
-    Gender VARCHAR(10),
-    DepartmentID INT
-);
-
-INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID)
-VALUES
-(1001, 'Arun', 'Male', 101),
-(1002, 'Divya', 'Female', 102),
-(1003, 'Karthik', 'Male', 101);
+CREATE TABLE Student ( StudentID INT PRIMARY KEY, StudentName VARCHAR(20) NOT NULL, Gender VARCHAR(10), DepartmentID INT );
+INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID) VALUES (1001, 'Arun', 'Male', 101), (1002, 'Divya', 'Female', 102), (1003, 'Karthik', 'Male', 101);
 UPDATE Student
-SET DepartmentID = 103
-WHERE StudentID = 1003;
-  
-DELETE from Student
+    SET DepartmentID = 103
+    WHERE StudentID = 1003; 
+
+DELETE FROM Student 
 WHERE StudentID = 1002;
-select * from Student;
+
+SELECT * FROM Student;
+
